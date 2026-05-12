34a94f2d47d259d76326ed98d995be4bd26445b9
Volonte Rwicha <volonterwicha2004@gmail.com>
2026-05-12 02:49:41 +0200

    add semi colon

--- redux-todo/src/components/TodoItem.jsx
+++ redux-todo/src/components/TodoItem.jsx
@@ -2,28 +2,33 @@ import { useDispatch, useSelector } from "react-redux";
 import { deleteTodo, toggleTodo } from "../features/todos";
 
 function TodoItem() {
-  const todos = useSelector((state)=> state.todo.items)
-  const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todo.items);
+  const dispatch = useDispatch();
   return (
     <>
-    {todos.map((todo)=> (
-      <li className={`todo-item ${todo.completed ? "completed" : ""}`} key={todo.id}>
-      <label className="todo-checkbox">
-        <input
-          type="checkbox"
-          checked={todo.completed}
-          onChange={() => dispatch(toggleTodo(todo.id))}
-        />
-        <span className="checkmark"></span>
-      </label>
-      <span className="todo-text">{todo.text}</span>
-      <button className="delete-btn" onClick={() => dispatch(deleteTodo(todo.id))}>
-        ✕
-      </button>
-    </li>
-    ))}
-     </>
-    
+      {todos.map((todo) => (
+        <li
+          className={`todo-item ${todo.completed ? "completed" : ""}`}
+          key={todo.id}
+        >
+          <label className="todo-checkbox">
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => dispatch(toggleTodo(todo.id))}
+            />
+            <span className="checkmark"></span>
+          </label>
+          <span className="todo-text">{todo.text}</span>
+          <button
+            className="delete-btn"
+            onClick={() => dispatch(deleteTodo(todo.id))}
+          >
+            ✕
+          </button>
+        </li>
+      ))}
+    </>
   );
 }
 
